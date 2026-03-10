0e98ff535e4a79ee67ca37e1b0904636a9d124c7
Sreyasi Munshi <sreyasi82@gmail.com>
2026-03-10 16:15:18 -0600

    Merge remote changes

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  app.use((req, res, next) => {
-    if (req.url.endsWith('.wasm')) {
-      res.setHeader('Content-Type', 'application/wasm');
-    }
-    next();
-  });
-};
