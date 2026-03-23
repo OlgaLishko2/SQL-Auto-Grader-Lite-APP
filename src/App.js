d229667018a032bf5c2bf5b36a8642b884ee62c3
Olga Lishko <lishkolga@gmail.com>
2026-03-23 14:23:22 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/home/Home"; 
-import Register from "./components/register/Register";
-import Login from "./components/login/Login";
-import About from "./components/about/About";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { auth, db } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import About from "./pages/about/About";
+import Footer from "./components/bars/Footer";
+import Profile from "./pages/profile/Profile";
+import NavBar from "./components/bars/Navbar";
+
+import Layout from "./pages/dashboard/layout/Layout";
+import Dashboard from "./pages/dashboard/Dashboard";
+// student
+import Assignments from "./pages/dashboard/student/assignments/Assignments";
+import QuestionList from "./pages/dashboard/student/assignments/QuestionList";
+import Quizzes from "./pages/dashboard/student/quizzes/Quizzes";
+import Results from "./pages/dashboard/student/results/Results";
+import SubmittedQuestions from "./pages/dashboard/student/results/SubmittedQuestions";
+//import AntiCheatingAssignmentDetail from "./pages/dashboard/student/assignments/AntiCheatingAssignmentDetail";
+// teacher
+import DatabaseLoader from "./pages/dashboard/teacher/datasets/dbLoader"
+import AssignmentForm from "./pages/dashboard/teacher/assignmentform/AssignmentForm"
+import AssignmentList from "./pages/dashboard/teacher/assignmentform/AssignmentList"
+import QuizForm from "./pages/dashboard/teacher/quizform/QuizForm"
+import QuizList from "./pages/dashboard/teacher/quizform/QuizList"
+import CohortManager from "./pages/dashboard/teacher/cohorts/CohortManager"
+import SubmissionStatusPage from "./pages/dashboard/teacher/submissionstatus/SubmissionStatusPage";
+
+
 import "./App.css";
 import AssignmentDetail from "./pages/dashboard/student/assignments/AntiCheatingQuestionDetail";
 import AntiCheatingQuestionDetail from "./pages/dashboard/student/assignments/AntiCheatingQuestionDetail";
@@ -57,6 +84,31 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
+
+
+            <Route path="/dashboard" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+              <Route index element={<Dashboard role={role}/>} />
+              <Route path="assignments" element={
+                role === "student"
+                  ? <Assignments />
+                  : <TeacherAssignments />
+              } />
+              <Route path="assignments/:id" element={<AssignmentDetail />} />
+              <Route path="questions/:assignment_id" element={<QuestionList />} />
+              <Route path="questions/:assignment_id/question-view/:question_id" element={<AssignmentDetail />} />
+              <Route path="quizzes" element={<Quizzes />} />
+              <Route path="results" element={<Results />} />
+              <Route path="results/:assignment_id" element={<SubmittedQuestions />} />
+              {/* <Route path="questions" element={<CreateQuestionSet />} /> */}
+              {/* <Route path="datasets" element={<Datasets />} /> */}
+              <Route path="datasets" element={<DatabaseLoader />} />
+              <Route path="cohorts" element={<CohortManager />} />
+              <Route path="submissionstatus" element={<SubmissionStatusPage />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+
+
+
             <Route path="/login" element={<Login />} />
             <Route path="/about" element={<About />} />
           </Routes>
